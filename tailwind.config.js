e780d6320f02bb25d2a3e1a01d066dee2a6c5404
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-02 16:05:11 -0300

    feat: configuração de cores e fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueChristmas: {
+          100: "#C8DDFF"
+        },
+        whiteChristmas: {
+          100: "#fff",
+          200: "#f2f3f3"
+        },
+        darkChristmas: {
+          100: "#565656",
+          200: "#404040"
+        }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }

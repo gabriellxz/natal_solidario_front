49bcb83bf374b61c1a576a5c563fd91d18973a3c
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-04 00:09:33 -0300

    feat: adicionando fontes e cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,20 @@ export default {
         },
         whiteChristmas: {
           100: "#fff",
-          200: "#f2f3f3"
+          200: "#f2f3f3",
+          300: "#F4F7FE",
+          400: "#A3AED0"
         },
         darkChristmas: {
           100: "#565656",
-          200: "#404040"
+          200: "#404040",
+          300: "#2B3674"
         }
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
-        inter: ["Inter", "sans-serif"]
+        inter: ["Inter", "sans-serif"],
+        dmsans: ["DM sans", "sans-serif"]
       }
     },
   },

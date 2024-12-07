05acf54d978711d91405a43ac72554dc631c647b
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-07 18:39:10 -0300

    style: responsividade dos cards

--- src/pages/Dashboard/TableCollectors/TableCollectors.tsx
+++ src/pages/Dashboard/TableCollectors/TableCollectors.tsx
@@ -2,12 +2,13 @@ import { Box, Card, CardContent, Typography } from "@mui/material";
 import food_icon from "../../../assets/food_icon.svg";
 import toys_icon from "../../../assets/toys_icon.svg";
 import clothing_icon from "../../../assets/clothing_icon.svg";
+import TableTargets from "../../../components/Dashboard/TableTargets";
 
 export default function TableCollectors() {
-    return (
-        <main className="mt-[50px]">
-            <div className="flex justify-around">
-                <Card className="w-full max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
+    return ( 
+        <main className="mt-[50px] w-full">
+            <div className="flex flex-col items-center gap-5 sm:flex-row sm:justify-around px-5">
+                <Card className="w-full sm:max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
                     <CardContent className="flex gap-5">
                         <img src={food_icon} alt="food_icon" className="bg-whiteChristmas-300 rounded-full p-3" />
                         <Box>
@@ -20,7 +21,7 @@ export default function TableCollectors() {
                         </Box>
                     </CardContent>
                 </Card>
-                <Card className="w-full max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
+                <Card className="w-full sm:max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
                     <CardContent className="flex gap-5">
                         <img src={toys_icon} alt="food_icon" className="bg-whiteChristmas-300 rounded-full p-3" />
                         <Box>
@@ -33,7 +34,7 @@ export default function TableCollectors() {
                         </Box>
                     </CardContent>
                 </Card>
-                <Card className="w-full max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
+                <Card className="w-full sm:max-w-[248px]" sx={{ display: "flex", alignItems: "center", borderRadius: 5 }}>
                     <CardContent className="flex gap-5">
                         <img src={clothing_icon} alt="food_icon" className="bg-whiteChristmas-300 rounded-full p-3" />
                         <Box>
@@ -46,6 +47,9 @@ export default function TableCollectors() {
                         </Box>
                     </CardContent>
                 </Card>
+            </div>
+            <div className="flex justify-center w-full px-[100px]">
+                <TableTargets/>
             </div>
         </main>
     )

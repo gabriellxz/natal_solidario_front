b4b9ced11fad1d2564009b970e3bf6da2652861a
José Wendel <wendelmarquesdahora@gmail.com>
2024-12-13 17:23:59 -0300

    feat: Responsividade da tabela de metas cumpridas em andamento

--- src/components/Dashboard/TableTargets.tsx
+++ src/components/Dashboard/TableTargets.tsx
@@ -1,66 +1,3 @@
-/*import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 20, 30, 24, 20),
-
-];
-
-export default function TableTargets() {
-  return (
-    <div className="bg-white mt-[94px] w-[1200px] ml-[80px] pb-[81px] border border-slate-500 rounded-3xl">
-      <div className="ml-[62px] mt-[29px] mr-[981px] mb-[25px] font-poppins font-[18px] font-semibold">
-        <span>Metas cumpridas</span>
-      </div>
-      <div className="mt-[61px]">
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Nome</TableCell>
-                <TableCell align="right">N° de Doadores</TableCell>
-                <TableCell align="right">N° de Doadores</TableCell>
-                <TableCell align="right">N° de Doadores</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    </div>
-  );
-}
-*/
-
 import { useState } from "react";
 import { Popover, Divider } from "@mui/material";
 import { EllipsisVertical } from "../../pages/DashboardCollectors/components/icons/EllipsisVertical";
@@ -89,7 +26,7 @@ export default function TableTargets() {
         <table className="w-full">
           <thead className="w-full">
             <tr className="border-b border-gray-300 w-full">
-              <th className="text-left sm:py-3 sm:px-6 md:pl-[46px] font-medium">
+              <th className="text-left sm:py-3 sm:px-6 md:pl-[46px] pl-[46px] font-medium">
                 Nome
               </th>
               <th className="sm:text-left sm:py-3 sm:px-6 font-medium">

a7a5ae8304a43c4f11dfbfdf858fcc51f5d312aa
José Wendel <wendelmarquesdahora@gmail.com>
2024-12-16 15:41:13 -0300

    feat: Finalizando responsividade da tabela de metas cumpridas

--- src/components/Dashboard/TableTargets.tsx
+++ src/components/Dashboard/TableTargets.tsx
@@ -18,37 +18,48 @@ export default function TableTargets() {
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
   return (
-    <div className="bg-white mt-[94px] max-w-[1200px] w-full pb-[81px] border border-gray-400 rounded-3xl">
+    <div className="bg-white mt-[94px] max-w-[1202px] w-full pb-[81px] border border-gray-400 rounded-3xl">
       <div className="p-[30px] pl-[70px] font-poppins text-lg font-medium">
         <span>Metas Cumpridas</span>
       </div>
-      <div className="mt-[61px] font-poppins w-full">
-        <table className="w-full">
+
+      {/* Wrapper com scroll horizontal */}
+      <div className="overflow-x-auto sm:overflow-visible">
+        <table className="w-full whitespace-nowrap">
           <thead className="w-full">
-            <tr className="border-b border-gray-300 w-full">
-              <th className="text-left sm:py-3 sm:px-6 md:pl-[46px] pl-[46px] font-medium">
+            <tr className="border-b border-gray-300 w-full md:text-[14px] text-[12px]">
+              <th className="text-left sm:py-3 sm:px-6 md:pl-[46px] pl-[33px] font-medium border-x border-gray-200 sm:border-x-0">
                 Nome
               </th>
-              <th className="sm:text-left sm:py-3 sm:px-6 font-medium">
-                N° de Doadores
+              <th className="sm:text-left sm:py-3 sm:px-6 font-medium border-x border-gray-200 sm:border-x-0">
+                <span className="block sm:inline">N°</span>
+                <span className="block sm:inline">de Doadores</span>
               </th>
-              <th className="sm:text-left sm:py-3 sm:px-6 font-medium">
-                N° de Doações
+              <th className="sm:text-left sm:py-3 sm:px-6 font-medium border-x border-gray-200 sm:border-x-0">
+                <span className="block sm:inline">N°</span>
+                <span className="block sm:inline">de Doações</span>
               </th>
-              <th className="sm:text-left sm:py-3 sm:px-6 font-medium">
-                N° de Alimentos
+              <th className="sm:text-left sm:py-3 sm:px-6 font-medium border-x border-gray-200 sm:border-x-0">
+                <span className="block sm:inline">N°</span>
+                <span className="block sm:inline">de Alimentos</span>
               </th>
             </tr>
           </thead>
           <tbody className="w-full">
-            <tr className="border-b border-gray-200">
-              <td className="py-6 px-6 pl-[46px] whitespace-nowrap">
+            <tr className="border-b border-gray-200 md:text-[14px] text-[12px] font-normal">
+              <td className="py-6 px-6 pl-[33px] md:pl-[46px] whitespace-nowrap border-x border-gray-200 sm:border-x-0">
                 Neto Betania
               </td>
-              <td className="py-6 px-6">20</td>
-              <td className="py-6 px-6">30</td>
-              <td className="py-6 px-6">20</td>
-              <td className="py-6 px-6">
+              <td className="py-6 px-6 border-x border-gray-200 sm:border-x-0">
+                20
+              </td>
+              <td className="py-6 px-6 border-x border-gray-200 sm:border-x-0">
+                30
+              </td>
+              <td className="py-6 px-6 border-x border-gray-200 sm:border-x-0">
+                20
+              </td>
+              <td className="py-6 px-6 border-x border-gray-200 sm:border-x-0">
                 <button aria-describedby={id} onClick={handleClick}>
                   <EllipsisVertical />
                 </button>
